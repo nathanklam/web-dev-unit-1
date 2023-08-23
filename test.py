abeebcea0d6d0e8d9385e5a092c9fc0223d68ad3
Nathan Lam <nathan.k.lam04@gmail.com>
2023-08-23 15:44:46 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+print("the missile knows where it is at all times. it knows this because it knows where it isnt")
+
+
+#not supposed to be ran here probably
+git add "test.py"
+git commit -m "first commit"
+git push
