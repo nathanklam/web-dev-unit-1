a18a54bc11b93e0de5493407c18c3d35a109faae
Nathan Lam <nathan.k.lam04@gmail.com>
2023-08-31 16:45:30 +0000

    finished probably

--- individual_Uemura.py
+++ individual_Uemura.py
@@ -1,52 +1,48 @@
-#not against computer i guess
-player1wins = 0
-player2wins = 0
+
 player1status = "alive"
 player2status = "alive"
+turn = 0
+turns = 5
+
+def get_player_choice(player_number):
+    while True:
+        choice = input(f"Player {player_number}, choose Rock, Paper, or Scissors: ").lower()
+        if choice in ["rock", "paper", "scissors", "gun", "break"]:
+            return choice
+        else:
+            print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+
+def determine_winner(player1_choice, player2_choice):
+    if player1_choice == "gun":
+        return "Player 2 has been killed"
+    elif player2_choice == "gun":
+        return "Player 1 has been killed"
+    elif player1_choice or player2_choice == "break":
+        return False
+    elif player1_choice == player2_choice:
+        return "It's a tie!"
+    elif player1_choice == "rock":
+        return "Player 1 wins!" if player2_choice == "scissors" else "Player 2 wins!"
+    elif player1_choice == "paper":
+        return "Player 1 wins!" if player2_choice == "rock" else "Player 2 wins!"
+    elif player1_choice == "scissors":
+        return "Player 1 wins!" if player2_choice == "paper" else "Player 2 wins!"
+
+        
+print("Welcome to completely normal rock-paper-scissors!")
+while turns <= turns and player1status == "alive" and player2status == "alive":
+    print(f"This is turn {turn} out of {turns}")
+    player1_choice = get_player_choice(1)
+    player2_choice = get_player_choice(2)
+    result = determine_winner(player1_choice, player2_choice)
+    if result == False:
+        break
+    if result != "It's a tie!":
+        turn += 1
+    if result == "Player 1 has been killed":
+        player1status = "dead"
+    elif result == "Player 2 has been killed":
+        player2status = "dead"
+    print(result)
 
 
-#def helper_function(player1input, player2input):
-
-
-def play_game():
-    player1 = input("Player 1, choose rock, paper, or scissors: ")
-    player2 = input("Player 2, choose rock, paper, or scissors: ")
-
-    #find out who wins
-    if player1 == player2:
-        print("tie, try again")
-    #this is stupid but uh; add knife, and pen stuff; these triumph over the normal game stuff
-    #instead of wins, it just kills the other person ???????
-    elif player1 == "gun":
-        player1wins += 1
-    elif player2 == "gun":
-        player1wins += 1
-    else:
-        print("i havent gotten to this point yet")
-    #now for the actual stuff
-    #pseudocode
-    #helper_function():
-        #if one is scissors and one is paper, the person with scissors gets a point
-        #if one is paper and one is rock, the person with paper gets a point
-        #if one is rock and one is scissors, the person with rock gets a point
-
-play_game()
-
-#print("welcome to a very normal game of rock paper scissors.")
-turns = 0
-#while turns <= 5:
-    #run game, internal parts of function should add 1 to "turns"
-
-    #compare who has the highest number of points at the end or whoever is still alive
-    #if (both players are still alive)
-        #if player1wins > player2wins
-            #
-        #elif player2wins < player1wins
-            #
-    #else (one is dead)
-        #if player1status == "dead"
-            #
-        #elif player2status == "dead"
-            #
-
-    #give the victory to that player
